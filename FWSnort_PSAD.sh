298a8dd722ae8295c392f0e75604813954bc6829
Mack6767 <35248695+Mack6767@users.noreply.github.com>
2020-05-30 23:42:01 -0700

    Update FWSnort_PSAD.sh

--- FWSnort_PSAD.sh
+++ FWSnort_PSAD.sh
@@ -1,6 +1,13 @@
 #Used with ideas @netson from https://gist.github.com/netson/c45b2dc4e835761fbccc
 
-sudo printf "# log kernel generated IPTABLES log messages to file
+
+sudo echo "# custom psad logging directives
+-A INPUT -j LOG
+-A FORWARD -j LOG
+-A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
+-A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"" >> /home/pi/Documents/iptables_Append.txt
+
+sudo echo "# log kernel generated IPTABLES log messages to file
 # each log line will be prefixed by "[IPTABLES]", so search for that
 :msg,contains,"[IPTABLES]" /var/log/iptables.log
 
@@ -8,9 +15,7 @@ sudo printf "# log kernel generated IPTABLES log messages to file
 # doing this will stop logging kernel generated IPTABLES log messages to the file
 # normally containing kern.* messages (eg, /var/log/kern.log)
 # older versions of ubuntu may require you to change stop to ~
-& stop" >  /etc/rsyslog.d/10-iptables.conf
-
- sudo service rsyslog restart
+& stop" >>  /etc/rsyslog.d/10-iptables.conf
 
 sudo nano /etc/psad/psad.conf
 
@@ -19,28 +24,14 @@ EXTERNAL_NET                !$HOME_NET;
 IPT_SYSLOG_FILE             /var/log/iptables.log;
 Change ENABLE_INTF_LOCAL_NETS to N
 
-printf '$-73i\n# custom psad logging directives\n
--A INPUT -j LOG\n
--A FORWARD -j LOG\n
--A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n
--A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n.\nw\n' | ed -s /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
-
-
-
-
-
-
+#printf '$-73i\n# custom psad logging directives\n
+#-A INPUT -j LOG\n
+#-A FORWARD -j LOG\n
+#-A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n
+#-A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n.\nw\n' | ed -s /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
 
 sudo ufw logging on
 sudo ufw allow ssh
-
-# custom psad logging directives
--A INPUT -j LOG
--A FORWARD -j LOG
--A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
--A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
-
-
 
 ############################################################
 FWSNORT
@@ -63,26 +54,12 @@ ufw logging on
 
 nano /etc/ufw/before.rules
 
-# custom psad logging directives
--A INPUT -j LOG
--A FORWARD -j LOG
--A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
--A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
-
 nano /etc/ufw/before6.rules
 
-# custom psad logging directives
--A INPUT -j LOG
--A FORWARD -j LOG
--A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
--A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
-
-
-
-printf "[Webmin]
+sudo echo "[Webmin]
 title=Webmin Portal (HTTPS)
 description=Webmin Portal (HTTPS)
-ports=10000/tcp" > /etc/ufw/applications.d/ufw-webmin
+ports=10000/tcp" >> /etc/ufw/applications.d/ufw-webmin
 
 
 ufw allow to any app Webmin
