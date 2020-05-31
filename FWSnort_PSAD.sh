f5ed5af9ef356a7fd36a887d0023ced50eb96c08
Mack6767 <35248695+Mack6767@users.noreply.github.com>
2020-05-31 00:58:57 -0700

    Update FWSnort_PSAD.sh

--- FWSnort_PSAD.sh
+++ FWSnort_PSAD.sh
@@ -1,40 +1,59 @@
 #Used with ideas @netson from https://gist.github.com/netson/c45b2dc4e835761fbccc
+#sed '1 a #This is just a commented line' sedtest.txt
+#sed '$ a This is the last line' sedtest.txt
 
+printf '\n%s\n' "Starting installation ......";
+{
+  sudo su
 
-sudo echo "# custom psad logging directives
--A INPUT -j LOG
--A FORWARD -j LOG
--A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"
--A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"" >> /home/pi/Documents/iptables_Append.txt
+  apt install ufw gufw -y
 
-sudo echo "# log kernel generated IPTABLES log messages to file
-# each log line will be prefixed by "[IPTABLES]", so search for that
-:msg,contains,"[IPTABLES]" /var/log/iptables.log
+  sed -i.bak '$ a\deb https://download.webmin.com/download/repository sarge contrib' /etc/apt/sources.list
+  cd /root
+  wget https://download.webmin.com/jcameron-key.asc
+  apt-key add jcameron-key.asc
 
-# the following stops logging anything that matches the last rule.
-# doing this will stop logging kernel generated IPTABLES log messages to the file
-# normally containing kern.* messages (eg, /var/log/kern.log)
-# older versions of ubuntu may require you to change stop to ~
-& stop" >>  /etc/rsyslog.d/10-iptables.conf
+  apt install apt-transport-https -y && apt update -y && apt install webmin -y
 
-sudo nano /etc/psad/psad.conf
+  echo "[Webmin]
+  title=Webmin Portal (HTTPS)
+  description=Webmin Portal (HTTPS)
+  ports=10000/tcp" >> /etc/ufw/applications.d/ufw-webmin
+
+  ufw allow to any app Webmin
+  ufw logging on
+  ufw allow ssh
+  ufw logging on
+
+  sed -i.bak '/processed/i\# custom psad logging directives\n
+  -A INPUT -j LOG\n
+  -A FORWARD -j LOG\n
+  -A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n
+  -A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n' /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
+
+  echo "# log kernel generated IPTABLES log messages to file
+  # each log line will be prefixed by "[IPTABLES]", so search for that
+  :msg,contains,"[IPTABLES]" /var/log/iptables.log
+  
+  # the following stops logging anything that matches the last rule.
+  # doing this will stop logging kernel generated IPTABLES log messages to the file
+  # normally containing kern.* messages (eg, /var/log/kern.log)
+  # older versions of ubuntu may require you to change stop to ~
+  & stop" >> /etc/rsyslog.d/10-iptables.conf
+
+  # Make manual changes to psad.conf
+  sudo nano /etc/psad/psad.conf
+} &> /dev/null
 
 HOME_NET                    192.168.1.0/24;
 EXTERNAL_NET                !$HOME_NET;
 IPT_SYSLOG_FILE             /var/log/iptables.log;
 Change ENABLE_INTF_LOCAL_NETS to N
 
-#printf '$-73i\n# custom psad logging directives\n
-#-A INPUT -j LOG\n
-#-A FORWARD -j LOG\n
-#-A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n
-#-A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n.\nw\n' | ed -s /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
 
-sudo ufw logging on
-sudo ufw allow ssh
 
 ############################################################
-FWSNORT
+# FWSNORT
 ############################################################
 
 sudo nano /etc/fwsnort/fwsnort.conf
@@ -45,27 +64,5 @@ UPDATE_RULES_URL        http://rules.emergingthreats.net/open/snort-2.9.0/emergi
 
 sudo sed -i '18748 s/^/#/' /var/lib/fwsnort/fwsnort.save
 sudo /var/lib/fwsnort/fwsnort.sh
-
-sudo su
-
-apt install ufw gufw -y
-
-ufw logging on
-
-nano /etc/ufw/before.rules
-
-nano /etc/ufw/before6.rules
-
-sudo echo "[Webmin]
-title=Webmin Portal (HTTPS)
-description=Webmin Portal (HTTPS)
-ports=10000/tcp" >> /etc/ufw/applications.d/ufw-webmin
-
-
-ufw allow to any app Webmin
-
 fwsnort --update-rules
 fwsnort -N --ipt-sync
-
-nano /etc/ufw/applications.d/ufw-webmin
-
