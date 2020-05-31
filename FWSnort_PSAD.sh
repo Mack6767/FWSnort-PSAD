fdc78c22e1a03e37132311d50552720725afb684
Mack6767 <35248695+Mack6767@users.noreply.github.com>
2020-05-31 01:24:29 -0700

    Update FWSnort_PSAD.sh

--- FWSnort_PSAD.sh
+++ FWSnort_PSAD.sh
@@ -2,11 +2,9 @@
 #sed '1 a #This is just a commented line' sedtest.txt
 #sed '$ a This is the last line' sedtest.txt
 
-printf '\n%s\n' "Starting installation ......";
+printf '\n%s\n' "Starting Webmin Installation ......";
 {
   sudo su
-
-  apt install ufw gufw -y
 
   sed -i.bak '$ a\deb https://download.webmin.com/download/repository sarge contrib' /etc/apt/sources.list
   cd /root
@@ -14,23 +12,35 @@ printf '\n%s\n' "Starting installation ......";
   apt-key add jcameron-key.asc
 
   apt install apt-transport-https -y && apt update -y && apt install webmin -y
+  
+  exit
+} &> /dev/null
+printf '\n%s\n' "Starting UFW and GUFW Installation and configuration ......";
+{
+  sudo su
+  apt install ufw gufw -y
+
+  ufw logging on
+  ufw allow ssh
+  ufw logging on
+
+# custom psad logging directives for iptables
+
+  sed -i.bak '/processed/i\# custom psad logging directives\n
+ # Included in case you don't want to use --log-tcp-options
+  # -A INPUT -j LOG\n
+ # Included in case you don't want to use --log-tcp-options
+  # -A FORWARD -j LOG\n
+  -A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n
+  -A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n' /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
 
   echo "[Webmin]
   title=Webmin Portal (HTTPS)
   description=Webmin Portal (HTTPS)
   ports=10000/tcp" >> /etc/ufw/applications.d/ufw-webmin
-
+  
   ufw allow to any app Webmin
-  ufw logging on
-  ufw allow ssh
-  ufw logging on
-
-  sed -i.bak '/processed/i\# custom psad logging directives\n
-  -A INPUT -j LOG\n
-  -A FORWARD -j LOG\n
-  -A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n
-  -A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n' /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
-
+  
   echo "# log kernel generated IPTABLES log messages to file
   # each log line will be prefixed by "[IPTABLES]", so search for that
   :msg,contains,"[IPTABLES]" /var/log/iptables.log
@@ -40,17 +50,20 @@ printf '\n%s\n' "Starting installation ......";
   # normally containing kern.* messages (eg, /var/log/kern.log)
   # older versions of ubuntu may require you to change stop to ~
   & stop" >> /etc/rsyslog.d/10-iptables.conf
-
-  # Make manual changes to psad.conf
-  sudo nano /etc/psad/psad.conf
+  service rsyslog restart
+  exit
 } &> /dev/null
+printf '\n%s\n' "Starting PSAD configuration Installation ......";
+{
+# Make manual changes to psad.conf
+sudo nano /etc/psad/psad.conf
+
 
 HOME_NET                    192.168.1.0/24;
 EXTERNAL_NET                !$HOME_NET;
 IPT_SYSLOG_FILE             /var/log/iptables.log;
-Change ENABLE_INTF_LOCAL_NETS to N
-
-
+ENABLE_INTF_LOCAL_NETS         N;
+EXPECT_TCP_OPTIONS             Y;
 
 ############################################################
 # FWSNORT
