beeb4a4f0c02573a948a104c288be893c77f8c85
Mack6767 <35248695+Mack6767@users.noreply.github.com>
2020-07-11 04:55:54 -0700

    Update FWSnort_PSAD.sh
    Reworked almost all of the script, this pain of a project is why I am glad I'm not a programmer.

--- FWSnort_PSAD.sh
+++ FWSnort_PSAD.sh
@@ -1,7 +1,13 @@
 #Used with ideas @netson from https://gist.github.com/netson/c45b2dc4e835761fbccc
-#sed '1 a #This is just a commented line' sedtest.txt
-#sed '$ a This is the last line' sedtest.txt
 #!/bin/bash
+touch /home/${USER}/Documents/webmin_install.log
+touch /home/${USER}/Documents/rsyslog_diag.log
+touch /home/${USER}/Documents/ufw_install.log
+touch /home/${USER}/Documents/fwsnort_install.log
+touch /home/${USER}/Documents/psad_install.log
+echo "Would you like to install Webmin? Please type Yes or No, followed by [ENTER]"
+read install
+if [[ $install == "Yes" ]]; then
 printf '\n%s\n' "Starting Webmin Installation ......";
 {
   sudo su
@@ -11,107 +17,104 @@ printf '\n%s\n' "Starting Webmin Installation ......";
   apt-key add jcameron-key.asc
   apt install apt-transport-https -y && apt update -y && apt install webmin -y
   exit
-} &> /dev/null
-
-printf '\n%s\n' "Starting UFW and GUFW Installation and configuration ......";
+} &> /home/${USER}/Documents/webmin_install.log
+elif [[ $install == "No" ]]; then
+printf '\n%s\n' "Starting UFW and GUFW Installation and configuring for FWSnort with PSAD ......";
+echo "First things first, lets get your network information. Please enter your nework in CIDR notation (192.168.1.0/24), followed by [ENTER]:"
+read network
 {
-  echo "First things first, lets get your network information. Please enter your nework in CIDR notation (192.168.1.0/24), followed by [ENTER]:"
-  read network
-  sudo apt install ufw gufw -y
-  sudo ufw allow from $network app SSH
-  sudo ufw logging on
-  sudo sed -i.bak '/processed/i\# custom psad logging directives\n# Included in case you do not want to use --log-tcp-options\n# sudo iptables -A INPUT -j LOG\n# Included in case you do not want to use --log-tcp-options\n# sudo iptables -A FORWARD -j LOG\n-A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n-A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n' /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
-
-  echo "[Webmin]
-  title=Webmin Portal (HTTPS)
-  description=Webmin Portal (HTTPS)
-  ports=10000/tcp" >> /etc/ufw/applications.d/ufw-webmin
-
-  sudo ufw allow from $network app Webmin
-
+  sudo su
+  apt install ufw gufw -y
+  ufw allow from $network app SSH
+  ufw logging on
+  sed -i.bak '/processed/i\# custom psad logging directives\n-A INPUT -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n-A FORWARD -j LOG --log-tcp-options --log-prefix "[IPTABLES]"\n' /etc/ufw/before.rules /etc/ufw/before6.rules /etc/ufw/after.rules /etc/ufw/after6.rules
+  if [[ $install == "Yes" ]]; then
+    ufw allow from $network app Webmin
+    echo "[Webmin]
+    title=Webmin Portal (HTTPS)
+    description=Webmin Portal (HTTPS)
+    ports=10000/tcp" >> /etc/ufw/applications.d/ufw-webmin
+  elif [[ $install == "No" ]]; then
+    echo "Webmin not installed via this script, not including rule."
+  fi
   echo "# log kernel generated IPTABLES log messages to file
   # each log line will be prefixed by "[IPTABLES]", so search for that
   :msg,contains,"[IPTABLES]" /var/log/iptables.log
-
   # the following stops logging anything that matches the last rule.
   # doing this will stop logging kernel generated IPTABLES log messages to the file
   # normally containing kern.* messages (eg, /var/log/kern.log)
   # older versions of ubuntu may require you to change stop to ~
   & stop" >> /etc/rsyslog.d/10-iptables.conf
-
-  printf '\n%s\n' "Checking if rsyslog is running ....";
-  {
- # ps -C httpd >/dev/null && echo "Running" || echo "Not running"
-  if ps -C httpd >/dev/null
-    then
+  exit
+} &> /home/${USER}/Documents/ufw_install.log
+printf '\n%s\n' "Checking if rsyslog is running ....";
+{
+  sudo ps -C httpd >/dev/null && echo "Running" || echo "Not running"
+  read check_one
+  if [[ $check_one == "Running" ]]; then
       echo "Service is running"
-      sudo service rsyslog restart >/dev/null && echo "rsyslog service has been restarted"
-    else
-      echo "Service is not running" && printf '\n%s\n' "Attempting to start rsyslog service ....";
-      sudo service rsyslog start >/dev/null
-    then
-      ps -C httpd >/dev/null && echo "Running" || echo "Not running"
-      read answer
-    if $answer is "Running"
-      then
-        sudo service rsyslog restart >/dev/null && echo "rsyslog service has been restarted"
-    else 
-      if $answer is "Not Running"
-        then
-          sudo systemctl list-unit-files | grep rsyslog
-          echo "Does it show as 'masked'? Type Yes or No, followed by [ENTER]:"
-          read masked
-            if $masked is "Yes"
-              then
-                sudo systemctl unmask rsyslog
-                sudo service rsyslog start
-            else
-              if $masked is "No"
-                then
-                  sudo service rsyslog start && ps -C httpd >/dev/null && echo "Running" || echo "Not running"
-                  read second_check
-                if $second_check is "Not running" 
-                  then
-                    printf '\n%s\n' "Further diagnostics are required, start with journalctl -xe | grep rsyslog ...."
-                else
-                  end
-                fi
+  elif [[ $check_one == "Not running" ]]; then
+    echo "Service is not running" && printf '\n%s\n' "Attempting to start rsyslog service ....";
+    sudo service rsyslog start >/dev/null && ps -C httpd >/dev/null && echo "Running" || echo "Not running"
+    read result
+      if [[ $result == "Running" ]]; then
+        echo "rsyslog service has been started"
+      elif [[ $result == "Not running" ]]; then
+        sudo systemctl list-unit-files | grep rsyslog
+        echo "Does it show as 'masked'? Type Yes or No, followed by [ENTER]:"
+        read masked
+          if [[ $masked == "Yes" ]]; then
+            sudo systemctl unmask rsyslog
+            sudo service rsyslog start
+          elif [[ $masked == "No" ]]; then
+            sudo service rsyslog start && sudo ps -C httpd >/dev/null && echo "Running" || echo "Not running"
+            read check_two
+              if [[ $check_two == "Not running" ]]; then
+                printf '\n%s\n' "Further diagnostics are required, start with journalctl -xe | grep rsyslog ...." && end
               fi
-            fi
-        fi
+          fi
       fi
-  #service rsyslog restart
-  #exit
-  } &> /dev/null
-} &> /dev/null
+  fi
+} &> /home/${USER}/Documents/rsyslog_diag.log
+fi
+printf '\n%s\n' "Starting FWSnort configuration ......";
+{
+  sudo su
+  apt install fwsnort -y
+  echo "Make manual changes to fwsnort.conf located at /etc/fwsnort/fwsnort.conf:
+  HOME_NET                YOUR_NETWORK_CIDR; #EX: 192.168.0.0/24
+  EXTERNAL_NET            !$HOME_NET; #Denotes anything not within your network CIDR
+  Add the following two lines to UPDATE_RULES_URL:
+  UPDATE_RULES_URL        http://rules.emergingthreats.net/open/snort-edge/emerging-all.rules;
+  UPDATE_RULES_URL        http://rules.emergingthreats.net/fwrules/emerging-IPTABLES-ALL.rules;"
+  fwsnort --update-rules
+  fwsnort -N --ipt-sync
+  /sbin/iptables-restore < /var/lib/fwsnort/fwsnort.save
+  exit
+} &> /home/${USER}/Documents/fwsnort_install.log
+printf '\n%s\n' "Starting PSAD configuration ......";
+{
+  sudo su
+  apt install psad -y
+  echo "Make manual changes to psad.conf located at /etc/psad/psad.conf
+  HOME_NET                    YOUR_NETWORK_CIDR; #EX: 192.168.0.0/24
+  EXTERNAL_NET                !$HOME_NET; #Denotes anything not within your network CIDR
+  IPT_SYSLOG_FILE             /var/log/iptables.log; #Points to the new log created earlier
+  ENABLE_INTF_LOCAL_NETS         N; #Prevents PSAD from assuming the network automatically
+  EXPECT_TCP_OPTIONS             Y;"
+  psad -K
+  psad --fw-include-ips
+  exit
+} &> /home/${USER}/Documents/psad_install.log
 
-#printf '\n%s\n' "Starting PSAD configuration Installation ......";
-#{
-# Make manual changes to psad.conf
-# sudo nano /etc/psad/psad.conf
+#service rsyslog restart
+#exit
 
+# If you go the fwsnort.sh route, you're going to need this:
+#sudo sed -i '18748 s/^/#/' /var/lib/fwsnort/fwsnort.save 
+# Change the number to whatever line is having issues because IPTables is cray cray, rinse and repeat.
 
-#HOME_NET                    $network;
-#EXTERNAL_NET                !$HOME_NET;
-#IPT_SYSLOG_FILE             /var/log/iptables.log;
-#ENABLE_INTF_LOCAL_NETS         N;
-#EXPECT_TCP_OPTIONS             Y;
-
-############################################################
-# FWSNORT
-############################################################
-
-#sudo nano /etc/fwsnort/fwsnort.conf
-
-#UPDATE_RULES_URL        http://rules.emergingthreats.net/open/snort-edge/emerging-all.rules;
-#UPDATE_RULES_URL        http://rules.emergingthreats.net/fwrules/emerging-IPTABLES-ALL.rules;
-#UPDATE_RULES_URL        http://rules.emergingthreats.net/open/snort-2.9.0/emerging-all.rules;
-
-#sudo sed -i '18748 s/^/#/' /var/lib/fwsnort/fwsnort.save
-
-#sudo /var/lib/fwsnort/fwsnort.sh
-
-#fwsnort --update-rules
-
-#fwsnort -N --ipt-sync
-#} &> /dev/null
+##      sudo service rsyslog restart >/dev/null && echo "rsyslog service has been restarted"
+#sed '1 a #This is just a commented line' sedtest.txt
+#sed '$ a This is the last line' sedtest.txt
+# ps -C httpd >/dev/null && echo "Running" || echo "Not running"
